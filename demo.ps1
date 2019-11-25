ee2ea2e7202e49b8bdeb78018543436d3aa729ea
Andreas Styltsvig <ADYS@oticonmedical.com>
2019-11-25 16:27:49 +0100

    fix

--- demo.ps1
+++ demo.ps1
@@ -2,9 +2,9 @@ git checkout -b demo-1 --track origin/master
 touch test
 git add test
 git commit -m "Testcommit"
-git push origin/demo-1
+git push origin demo-1
 git checkout -b demo-2 --track origin/demo-1
-git touch test2
+touch test2
 git add test2
 git commit -m "Testcommit 2"
 Start-Process Integration-robot.ps1
