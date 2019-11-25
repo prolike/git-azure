95d2cacc8babd1ad817415a51feef43f5c9da142
Andreas Styltsvig <ADYS@oticonmedical.com>
2019-11-25 15:45:45 +0100

    fix

--- test.ps1
+++ test.ps1
@@ -3,7 +3,11 @@ $currentBranchName = git branch | grep \* | cut -d ' ' -f2-
 Write-Host $currentBranchName
 
 try{
-	git rebase test origin/master 2>&1 | Write-Host
+	git checkout $currentBranchName
+	git rebase master 2>&1 | Write-Host
+	git checkout master
+	git merge $currentBranchName --ff-only
+	git push origin master
 }
 catch{
 	Write-Host "ERROR CATHCED"
